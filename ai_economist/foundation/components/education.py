e9108ee04c8c90b91b48a894db321dba4c101536
xforeverjlx <johannchrislee@gmail.com>
2021-10-26 18:29:14 -0400

    first half of go to school

---
+++ ai_economist/foundation/components/education.py
@@ -0,0 +1,117 @@
+import numpy as np
+from ai_economist.foundation.base.base_component import BaseComponent, component_registry
+@component_registry.add
+
+class GetEducated(BaseComponent):
+  name = "GetEducated"
+  required_entities = ["Coin", "Labor", "Skill"]  # <--- We can now look up "Widget" in the resource registry
+  agent_subclasses = ["BasicMobileAgent"]
+
+  def __init__(
+      self,
+      *base_component_args,
+      tuition=100, # same tuition cost as building 10 houses <- tweak later
+      build_labor=100.0,
+      **base_component_kwargs
+  ):
+      super().__init__(*base_component_args, **base_component_kwargs)
+      self.tuition = int(tuition)
+      assert self.tuition >= 0
+      self.build_labor = float(build_labor)
+      assert self.build_labor >= 0
+      # self.skill = int(skill)
+
+  def agent_can_get_educated(self, agent):
+    """Return True if agent can actually get educated."""
+    # See if the agent has the resources necessary to complete the action
+    
+    if agent.state["inventory"]["Coin"] < self.tuition:
+        return False
+
+    # Do nothing if skill is already max
+    if True: # TODO see how to get skill
+        return False
+
+    # If we made it here, the agent can go to college.
+    return True
+
+  def get_additional_state_fields(self, agent_cls_name):
+    if agent_cls_name not in self.agent_subclasses:
+        return {}
+    if agent_cls_name == "BasicMobileAgent":
+        return {"tuition_payment": float(self.tuition)} # check
+    raise NotImplementedError
+
+  def additional_reset_steps(self):
+      self.available_wood_units = 0
+
+  def get_n_actions(self, agent_cls_name):
+      if agent_cls_name == "BasicMobileAgent":
+          return 1
+      return None
+
+  def generate_masks(self, completions=0):
+      masks = {}
+      for agent in self.world.agents:
+          masks[agent.idx] = np.array([
+              agent.state["inventory"]["Coin"] >= self.widget_price and self.available_widget_units > 0
+          ])
+
+      return masks
+
+  def component_step(self):
+      """
+      See base_component.py for detailed description.
+      Convert coin to skill for agents that choose to go to school and can.
+      """
+      
+      world = self.world
+      build = []
+      # Apply any go_to_school actions taken by the mobile agents
+      for agent in world.get_random_order_agents():
+
+          action = agent.get_component_action(self.name)
+
+          # This component doesn't apply to this agent!
+          if action is None:
+              continue
+
+          # NO-OP!
+          if action == 0:
+              pass
+
+          # Learn! (If you can.)
+          elif action == 1:
+              if self.agent_can_get_educated(agent):
+                  # Remove the resources
+                  agent.state["inventory"]["Coin"] -= self.tuition
+
+                  # Receive skills for going to school
+                  # agent.state["inventory"]["Coin"] += agent.state["build_payment"]
+                  # TODO
+
+                  # Incur the labor cost for building
+                  agent.state["endogenous"]["Labor"] += self.build_labor
+
+                  build.append(
+                      {
+                          "builder": agent.idx,
+                          "loc": np.array(agent.loc),
+                          "income": float(agent.state["build_payment"]),
+                      }
+                  )
+          else:
+              raise ValueError
+
+      self.builds.append(build)
+
+  def generate_observations(self):
+      obs_dict = dict()
+      for agent in self.world.agents:
+          obs_dict[agent.idx] = {
+              "widget_refresh_rate": self.widget_refresh_rate,
+              "available_widget_units": self.available_widget_units,
+              "widget_price": self.widget_price
+          }
+
+      return obs_dict
